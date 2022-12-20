3dc1f173122c1c6d7f81a09a040abbc634a5727e
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2022-12-20 03:12:51 +0100

    Reworked Modal component

--- xml_editor/src/components/Modal.jsx
+++ xml_editor/src/components/Modal.jsx
@@ -3,8 +3,9 @@ import "./Modal.css";
 import { useContext } from "react";
 import { StateContext } from "../contexts/StateContext.jsx";
 import { CLOSE_MODAL } from "../reducers/AppReducer.js";
+import CloseIcon from '@mui/icons-material/Close';
 
-function Modal({ children }) {
+function Modal({ children, title, className }) {
     const { state, dispatch } = useContext(StateContext);
 
     if (!state.isModalOpen) return null;
@@ -16,9 +17,14 @@ function Modal({ children }) {
 
     return ReactDOM.createPortal(
         <>
-            <div className="modal__overlay"></div>
-            <div className="modal">
-                <button onClick={handleModalClose}>Close</button>
+            <div className="modal__overlay" onClick={handleModalClose}></div>
+            <div className={`${className} modal`}>
+                <header>
+                    <h1>{title}</h1>
+                    <button onClick={handleModalClose}>
+                        <CloseIcon fontSize="large" />
+                    </button>
+                </header>
                 {children}
             </div>
         </>,
