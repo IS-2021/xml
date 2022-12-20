91899b39ad973d6d0fb6e576767b707627738df0
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2022-12-21 00:34:38 +0100

    Defined schemas in external files

--- xml_editor/src/components/AddClientForm.jsx
+++ xml_editor/src/components/AddClientForm.jsx
@@ -1,7 +1,7 @@
-import { z } from "zod";
 import {Button, createTheme, Stack, TextField, ThemeProvider} from "@mui/material";
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {clientSchema} from "../xml/schemas/klient.js";
 
 const darkTheme = createTheme({
     palette: {
@@ -12,18 +12,12 @@ const darkTheme = createTheme({
     },
 });
 
-const newClientSchema = z.object({
-    imie: z.string().min(3, "Imię musi mieć min. 3 znaki"),
-    nazwisko: z.string().min(3, "Nazwisko musi mieć min. 3 znaki"),
-    pesel: z.string().length(11, "Pesel musi mieć dokładnie 11 znaków"),
-    login: z.string().max(20, "Login powinien mieć max. 20 znaków").optional()
-})
 
 function AddClientForm() {
     const { control, handleSubmit, formState: { errors }} = useForm({
         mode: "all",
         reValidateMode: "onChange",
-        resolver: zodResolver(newClientSchema),
+        resolver: zodResolver(clientSchema),
     });
     const onSubmit = data => console.log(data);
 
@@ -70,8 +64,8 @@ function AddClientForm() {
                     render={({ field }) => (
                         <TextField
                             {...field}
-                            label="Pesel"
-                            placeholder="Podaj pesel"
+                            label="PESEL / NIP"
+                            placeholder="Podaj PESEL lub NIP"
                             error={field.isDirty || !!errors[field.name]}
                             helperText={
                                 field.isDirty || !!errors[field.name] ? errors[field.name].message : ""
