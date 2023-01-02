6f5e8fe0947f4e561ef9e7bc71bfa59aab483455
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-01-02 11:19:04 +0100

    Implemented accordions

--- xml_editor/src/components/forms/tabs/klient/ClientRentTab.jsx
+++ xml_editor/src/components/forms/tabs/klient/ClientRentTab.jsx
@@ -1,7 +1,7 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { tabPropTypes } from "../propTypes.js";
 import TabPanel from "../TabPanel.jsx";
-import { Button, Stack } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Button, Stack } from "@mui/material";
 import { FormContext } from "../../../../contexts/FormContext.jsx";
 import { useFieldArray } from "react-hook-form";
 import { emptyWypozyczenie } from "../../initialFormData.js";
@@ -13,16 +13,43 @@ function ClientRentTab({ currentIndex, tabIndex }) {
         control,
         name: "wypozyczenia",
     });
+    const [expanded, setExpanded] = useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
+    const createNewRent = () => {
+        append(emptyWypozyczenie);
+    };
+
+    const removeRent = (idx) => {
+        remove(idx);
+        setExpanded(false);
+    };
+
+    useEffect(() => {}, [fields.length]);
 
     return (
         <TabPanel currentIndex={currentIndex} tabIndex={tabIndex}>
             <Stack spacing={1.5}>
-                <Button variant="outlined" fullWidth onClick={() => append(emptyWypozyczenie)}>
+                <Button variant="outlined" fullWidth onClick={createNewRent}>
                     Nowe wypożyczenie
                 </Button>
-                {fields.map((item, idx) => (
-                    <Rent rentIndex={idx} onDelete={() => remove(idx)} key={`RENT_${idx}`} />
-                ))}
+                <div style={{ height: "510px", overflowY: "scroll" }}>
+                    {fields.map((item, idx) => (
+                        <Accordion
+                            expanded={expanded === `panel${idx}`}
+                            onChange={handleChange(`panel${idx}`)}
+                            key={`RENT_${idx}`}
+                        >
+                            <AccordionSummary>Wypożyczenie ({item.albumy.length})</AccordionSummary>
+                            <AccordionDetails>
+                                <Rent rentIndex={idx} onDelete={() => removeRent(idx)} />
+                            </AccordionDetails>
+                        </Accordion>
+                    ))}
+                </div>
             </Stack>
         </TabPanel>
     );
