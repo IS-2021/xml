1f0f4ec76d2517f37cea333ce5d1c1a880ea8502
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-01-02 01:12:53 +0100

    Added schema for album rental

--- xml_editor/src/xml/schemas/klient.js
+++ xml_editor/src/xml/schemas/klient.js
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { nipSchema, peselSchema } from "./common.js";
+import { albumIDSchema, nipSchema, peselSchema } from "./common.js";
 
 const emailSchema = z
     .string()
@@ -7,9 +7,20 @@ const emailSchema = z
     .regex(/\w+@\w+\.\w+/, "Email nie spełnia wzoru");
 const nickSchema = z.string().min(3).max(16);
 
+const wypozyczenieSchema = z.object({
+    dataRozpoczecia: z.any(),
+    dataZakonczenia: z.any(),
+    albumy: z.array(
+        z.object({
+            numer: albumIDSchema,
+        })
+    ),
+});
+
 export const clientSchema = z.object({
     imie: z.string().min(3, "Imię musi mieć min. 3 znaki"),
     nazwisko: z.string().min(3, "Nazwisko musi mieć min. 3 znaki"),
     pesel: z.union([peselSchema, nipSchema]),
     login: z.union([emailSchema, nickSchema]),
+    wypozyczenia: z.array(wypozyczenieSchema),
 });
