f0b12419a8c9cfe1d5e6cbc9cfeb01c4e163fb88
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-01-02 12:53:54 +0100

    Cannot rent album if there's no albums present

--- xml_editor/src/components/forms/tabs/klient/ClientRentTab.jsx
+++ xml_editor/src/components/forms/tabs/klient/ClientRentTab.jsx
@@ -1,19 +1,30 @@
 import { useContext, useEffect, useState } from "react";
 import { tabPropTypes } from "../propTypes.js";
 import TabPanel from "../TabPanel.jsx";
-import { Accordion, AccordionDetails, AccordionSummary, Button, Stack } from "@mui/material";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Button,
+    Stack,
+    Tooltip,
+} from "@mui/material";
 import { FormContext } from "../../../../contexts/FormContext.jsx";
 import { useFieldArray } from "react-hook-form";
 import { emptyWypozyczenie } from "../../initialFormData.js";
 import Rent from "./Rent.jsx";
+import { StateContext } from "../../../../contexts/StateContext.jsx";
 
 function ClientRentTab({ currentIndex, tabIndex }) {
+    const { state } = useContext(StateContext);
     const { control } = useContext(FormContext);
     const { fields, append, remove } = useFieldArray({
         control,
         name: "wypozyczenia",
     });
     const [expanded, setExpanded] = useState(false);
+
+    const newRentDisabled = state.xml.refs.albumy.length === 0;
 
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded ? panel : false);
@@ -33,9 +44,18 @@ function ClientRentTab({ currentIndex, tabIndex }) {
     return (
         <TabPanel currentIndex={currentIndex} tabIndex={tabIndex}>
             <Stack spacing={1.5}>
-                <Button variant="outlined" fullWidth onClick={createNewRent}>
-                    Nowe wypożyczenie
-                </Button>
+                <Tooltip title={newRentDisabled && "Brak albumów do wypożyczenia"}>
+                    <span>
+                        <Button
+                            variant="outlined"
+                            fullWidth
+                            disabled={newRentDisabled}
+                            onClick={createNewRent}
+                        >
+                            Nowe wypożyczenie
+                        </Button>
+                    </span>
+                </Tooltip>
                 <div style={{ height: "510px", overflowY: "scroll" }}>
                     {fields.map((item, idx) => (
                         <Accordion
